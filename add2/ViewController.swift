dd5ce03d70b7091cd7317bee82d2dfd00275e317
mayue20121104992 <admin@admindeiMac-36.local>
2016-05-19 13:22:51 +0800

    6
    6

--- add2/ViewController.swift
+++ add2/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var x: UITextField!
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
@@ -48,85 +48,20 @@ class ViewController: UIViewController {
         
     }
     
-    @IBOutlet weak var resultLabel: UILabel!
-    
-    //存储数字字符串
-    var operand1 :String = ""
-    var operand2 :String = ""
-    
-    //存储运算符字符串
-    var operator :String = ""
-    var operator1 :String = ""
     
     
-@@ -62,8 +31,33 @@ class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-    
-
-
-    //设置命令
-    @IBAction func didClicked(sender: UIButton) {
-        //获取按钮名称
-        //print("\(sender.currentTitle)")
-        
-        let value = sender.currentTitle
-        
-        if value == "+" || value == "-" || value == "×" || value == "÷"
-        {
-            //print("\(value)")
-            operator = value
-            operator1 = value!
-            return
-        }
-        else
-        else if value == "="
-        {
-            print("no value")
-            
-            
-            var result = 0
-            switch operator1
-            {
-            case "+":
-                result = Int(operand1)! + Int(operand2)!
-                //result = operand1.toInt()! + operand2.toInt()!
-            
-            case "-":
-                result = Int(operand1)! - Int(operand2)!
-                
-            case "×":
-                result = Int(operand1)! * Int(operand2)!
-                
-            case "÷":
-                result = Int(operand1)! / Int(operand2)!
-            
-                
-            case "C":
-                result = 0
-                
-            default:
-                result = 0
-            }
-            
-            resultLabel.text = "\(result)"
-            return
-        }
-        
-        
-        if operator1 == ""
-        {
-            operand1 = operand1 + value!
-            resultLabel.text = operand1
-        }
-        else
-        {
-            operand2 = operand2 + value!
-            resultLabel.text = operand2
-
-        }
-        
-    }
+}
